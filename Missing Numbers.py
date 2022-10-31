ffd1bddc9a12b74bb07d6efcea806fdb85994fbb
jaysmith13 <janeasmith13@gmail.com>
2022-10-31 17:30:51 -0600

    Update Missing Numbers.py

--- Missing Numbers.py
+++ Missing Numbers.py
@@ -10,4 +10,5 @@ def findMissingNumbers(n):
             output.append(i)
     return output
 
-listofNumbers
+listofNumbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+print(findMissingNumbers(listofNumbers))
