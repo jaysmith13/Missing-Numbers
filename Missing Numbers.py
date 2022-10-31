5818b6c69da1853a1ef6b6eee8d8831330c21037
jaysmith13 <janeasmith13@gmail.com>
2022-10-31 17:28:49 -0600

    Update Missing Numbers.py

--- Missing Numbers.py
+++ Missing Numbers.py
@@ -4,3 +4,10 @@ import numbers
 def findMissingNumbers(n):
     numbers = set(n)
     length = len(n)
+    output = []
+    for i in range(1, n[-1]):
+        if i not in numbers:
+            output.append(i)
+    return output
+
+listofNumbers
